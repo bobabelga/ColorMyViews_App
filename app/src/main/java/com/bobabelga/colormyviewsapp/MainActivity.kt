faa10f335a50a0600494d8c68bdb430134ff3cb1
belgacem abibakr <belgacemabibakr@gmail.com>
2021-05-31 12:27:40 +0100

    Exercise: Completed

--- app/src/main/java/com/bobabelga/colormyviewsapp/MainActivity.kt
+++ app/src/main/java/com/bobabelga/colormyviewsapp/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.bobabelga.colormyviewsapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bobabelga.colormyviewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    fun makeColored(view: View) {
+        when(view.id) {
+            R.id.box_one_txt -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_txt -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_txt -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_txt -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_txt -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_btn -> binding.boxThreeTxt.setBackgroundResource(R.color.my_red)
+            R.id.yellow_btn -> binding.boxFourTxt.setBackgroundResource(R.color.my_yellow)
+            R.id.green_btn -> binding.boxFiveTxt.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
